348b262f77dea34259daab7ddc0f5c33d1054bba
Nathan Peterson <nalexpeterson@gmail.com>
2016-11-29 19:12:46 -0600

    Create User Object

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,17 @@
-export class User {
+export class User{
+  id: number;
+  username: string;
+  email: string;
+  dateOfBirth: string;
+  address: string;
+  //Enum type for flag of type of user
+  //Password... string
+  gender: char;
+  discipline: string;
+  degreeOfProfessionalism: string; // Find better name
+
+
+  constructor(values: object{}){
+    Object.assign(this, values);
+  }
 }
